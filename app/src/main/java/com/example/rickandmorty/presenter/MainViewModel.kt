7f38abce3afd08c3f9ad71ab0c26ec4c155a9884
Bypv <bayron.pena@mercadolibre.com.co>
2022-05-31 20:28:15 -0500

    ah

--- app/src/main/java/com/example/rickandmorty/presenter/MainViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presenter/MainViewModel.kt
@@ -15,7 +15,6 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
             emit(repository.getCharacterApi())
         } catch (e: Exception) {
             Log.d("RemoteApiFailed:", "failed")
-            Log.d("RemoteApiFailed:", "failed")
             emit(Resource.Failure(e))
         }
     }
