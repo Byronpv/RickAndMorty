2ab4205542640f59d2f5550e53eb5c0e83a22ab6
Byronpv <byron19pv@gmail.com>
2022-08-19 19:47:18 -0500

    updatee

--- app/src/main/java/com/example/rickandmorty/data/api/ApiService.kt
+++ app/src/main/java/com/example/rickandmorty/data/api/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
-
+//test
     @GET("character")
     suspend fun getMultipleCharacters() : Response<CharacterListModel>
 }
